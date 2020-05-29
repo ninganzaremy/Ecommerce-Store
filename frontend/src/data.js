4f552ed4eac5d5c04a2968111caa7b55df0f57e2
Ningaanza Remy <remimaradona@gmail.com>
2020-05-28 20:05:25 -0400

    build the  data source

---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+export default {
+  products:[
+    {
+    name:'Stretch Dress',
+    category:'Shirts',
+    image: '/images/product0.jpg',
+    price:99,
+    brand:'Bar III',
+    rating:4.5,
+    numReview:188
+  },
+  {
+  name:'Stretch Dress',
+  category:'Shirts',
+  image: '/images/product0.jpg',
+  price:99,
+  brand:'Bar III',
+  rating:4.5,
+  numReview:188
+},
+    {
+    name:'Wool Suit Jacket',
+    category:'suits',
+    image: '/images/product1.jpg',
+    price:300,
+    brand:'Banana Republic',
+    rating:4.7,
+    numReview:10
+  },
+  {
+  name:'Groom Tuxedo',
+  category:'suits',
+  image: '/images/product2.jpg',
+  price:200,
+  brand:'Tuxedo',
+  rating:4.9,
+  numReview:20
+},
+{
+name:'Casual Slim',
+category:'Shrits',
+image: '/images/product3.jpg',
+price:60,
+brand:'Coton',
+rating:4.4,
+numReview:20
+},
+{
+name:'Casual Slim',
+category:'Shrits',
+image: '/images/product3.jpg',
+price:60,
+brand:'Coton',
+rating:4.4,
+numReview:20
+}
+]
+}
