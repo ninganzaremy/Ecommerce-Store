fddcbb5113c5d5a773ab3026f0e8c4366415931b
Ningaanza Remy <remimaradona@gmail.com>
2020-05-29 19:59:56 -0400

    fetching data from the server

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,11 +1,21 @@
-import React from "react";
-import data from "../data";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function HomePage(props) {
-  return (
-    <ul className="products">
-      {data.products.map((product) => (
+  const [products, setProduct]= useState([]);
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const {data} = await axios.get("/api/products");
+      setProduct(data)
+    }
+    fetchData();
+    return () => {
+      //
+    };
+  }, [])
+  return (<ul className="products">
+      {products.map((product) => (
         <li>
           <div className="product">
             <Link to={"/product/" + product._id}><img className="product-image" src={product.image}alt="product"></img>
