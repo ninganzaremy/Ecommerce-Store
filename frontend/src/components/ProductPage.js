b395c43eac83f87eeecf110a14b82851ebd9bfec
Ningaanza Remy <remimaradona@gmail.com>
2020-05-30 15:08:39 -0400

    fetch and display products quantity available in the stock

--- frontend/src/components/ProductPage.js
+++ frontend/src/components/ProductPage.js
@@ -1,59 +1,69 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { detailsProduct } from "../actions/productActions";
 
 
-function ProductPage(props){
-  console.log(props.match.params.id);
-  const product = data.products.find((x) => x._id === props.match.params.id);
-
+function ProductPage(props) {
+  const [qty, setQTY] = useState(1);
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+      //
+    };
+  }, []);
   return <div>
       <div className="back-to-result">
         <Link to="/">Back to result </Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt="product">
-            {" "}
-          </img>
-        </div>
-      </div>
+      {loading ? (
+        <div>loading....</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="product"></img>
+          </div>
 
-      <div className="details-info">
-        <ul>
-          <li>
-            <h4> {product.name}</h4>
-          </li>
-          <li>
-            {product.rating} Stars ({product.numReview} Reviews)
-          </li>
-          <li>
-            Price: <b>${product.price}</b>
-          </li>
-          <li>
-            Description:
-            <div>{product.description}</div>
-          </li>
-        </ul>
-      </div>
-      <div className="details-action">
-        <ul>
-          <li>Price: {product.price}</li>
-          <li>Status: {product.status}</li>
-          <li>
-            QTY:
-            <select>
-              <option>1</option>
-              <option>2</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </li>
-          <li>
-            <button>Add to cart</button>
-          </li>
-        </ul>
-      </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4> {product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReview} Reviews)
+              </li>
+              <li>
+                Price: <b>${product.price}</b>
+              </li>
+              <li>
+                Description:
+                <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>Price: {product.price}</li>
+              <li>Status: {product.status}</li>
+              <li>
+                QTY:
+                <select value={qty} onChange={(e) => {setQTY(e.target.value)}}>
+                {[...Array(product.countInStock).keys()].map(x=>
+                  <option value={x+1}>{x+1}</option>)}
+                </select>
+              </li>
+              <li>
+                <button className="button primary">Add to cart</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
 }
 export default ProductPage;
