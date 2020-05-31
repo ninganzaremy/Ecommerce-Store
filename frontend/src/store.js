4298ff490f03dd1915ac00c5ed23794cd9c9bfa3
Ningaanza Remy <remimaradona@gmail.com>
2020-05-30 20:50:59 -0400

    importing Cookie and saving all products in cart in case of browser refresh

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,26 @@
-import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import {productListReducer, productDetailsReducer} from './reducers/productReducers';
-import {cartReducer} from './reducers/cartReducers';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import Cookie from "js-cookie";
 
+const cartItems = Cookie.getJSON("cartItems") || [];
 
-
-const initialState= {};
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails :productDetailsReducer,
-  cart: cartReducer
-})
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer , initialState, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
