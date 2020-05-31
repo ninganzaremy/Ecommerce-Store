feaa314ef14eb1590aef7dd3cfe5f61a224c939c
Ningaanza Remy <remimaradona@gmail.com>
2020-05-31 19:51:58 -0400

    Setting server Route for products

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import data from './data';
 import dotenv from 'dotenv';
 import config from './config';
 import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+
 import mongoose  from 'mongoose';
 import bodyParser from 'body-parser';
 
@@ -19,20 +21,6 @@ mongoose.connect(mongodbUrl, {
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
-app.get("/api/products/:id", (req, res) =>{
-  const productId= req.params.id;
-  const product = data.products.find(x=>x._id == productId);
-  if (product) {
-    res.send(product);
-  }
-  else{
-    res.status(404).send({ msg: "Product Not Found"})
-  }
-
-});
-app.get("/api/products", (req, res) =>{
-  res.send(data.products);
-});
 app.listen(5000,() => {console.log("Server started at http://localhost:5000")});
