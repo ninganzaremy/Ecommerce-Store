816d06425da01ce4a8e1417571aa99a6bae08a56
Ningaanza Remy <remimaradona@gmail.com>
2020-05-30 23:43:33 -0400

    fixed the === signs

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,11 +4,11 @@ function cartReducer(state = { cartItems: [] }, action) {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
-      const product = state.cartItems.find((x) => x.product == item.product);
+      const product = state.cartItems.find((x) => x.product === item.product);
       if (product) {
         return {
           cartItems: state.cartItems.map((x) =>
-            x.product == product.product ? item : x
+            x.product === product.product ? item : x
           ),
         };
       }
