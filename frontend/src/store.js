59731ded6622069796b0b45499d01260f5c992f6
Ningaanza Remy <remimaradona@gmail.com>
2020-05-31 12:19:04 -0400

    UserSignin saved in our combineReducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,14 +6,18 @@ import {
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import Cookie from "js-cookie";
+import { userSigninReducer } from "./reducers/userReducers";
 
 const cartItems = Cookie.getJSON("cartItems") || [];
+const userInfo = Cookie.getJSON("userInfo") || null;
 
-const initialState = { cart: { cartItems } };
+
+const initialState = { cart: { cartItems } , userSignin:{userInfo}};
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
