2ff7a275add9a6e80f455bb323d030864e0370c6
Ningaanza Remy <remimaradona@gmail.com>
2020-05-31 09:59:41 -0400

    admin account creation

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,19 +2,38 @@ import express from "express";
 import User from '../models/userModel'
 
 const router = express.Router();
-
-router.get("/createadmin", async (req, res) => {
-    try {
-    const user = new User({
-      name: 'Remy',
-      email: 'remimaradona@gmail.com',
-      password: '1234',
-      isAdmin: true
+router.post('/signin', async (req, res) => {
+    const signinUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password
     });
-    const newUser = await user.save();
-    res.send(newUser);
-  } catch (error) {
-    res.send({ msg: error.message });
-  }
-});
+
+  if (signinUser) {
+      res.send({
+        _id: signinUser.id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: getToken(signinUser)
+      });
+
+    } else {
+      res.status(401).send({ msg: 'Invalid Email or Password.' });
+    }
+ });
+  router.get("/createadmin", async (req, res) => {
+      try {
+      const user = new User({
+        name: 'Remy',
+        email: 'remimaradona@gmail.com',
+        password: '1234',
+        isAdmin: true
+      });
+      const newUser = await user.save();
+      res.send(newUser);
+    } catch (error) {
+      res.send({ msg: error.message });
+    }
+  })
+
 export default router;
