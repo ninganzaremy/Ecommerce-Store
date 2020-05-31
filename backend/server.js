a10ad5b6916e233f7ff28db5178f180f7beb82a4
Ningaanza Remy <remimaradona@gmail.com>
2020-05-30 23:40:57 -0400

    connected to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import userRoute from "./routes/userRoute";
+import mongoose  from 'mongoose';
+
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .catch((error) => console.log(error.reason));
+
 
 const app = express();
+
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) =>{
   const productId= req.params.id;
   const product = data.products.find(x=>x._id == productId);
