781f25200de64a3aa3637fd1ff4fa5dc2727799c
Ningaanza Remy <remimaradona@gmail.com>
2020-05-31 13:14:26 -0400

    saving a successfully registered User

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,29 @@ import User from '../models/userModel';
 import { getToken, isAuth } from "../util";
 
 const router = express.Router();
+
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data." });
+  }
+});
+
+
 router.post('/signin', async (req, res) => {
     const signinUser = await User.findOne({
       email: req.body.email,
