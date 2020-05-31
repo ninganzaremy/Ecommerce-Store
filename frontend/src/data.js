ce9b27b4bf7cc02d96826ebc02ee05523db82f59
Ningaanza Remy <remimaradona@gmail.com>
2020-05-30 20:01:40 -0400

    changed product qty to 0 to make sure that the availability status change to OUT OF STOCK

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export default {
+  products: [
+    {
+      _id: 1,
+      name: 'Stretch Dress',
+      category: 'Shirts',
+      image: '/images/product0.jpg',
+      price: 99,
+      brand: 'Bar III',
+      rating: 4.5,
+      numReview: 188,
+      countInStock: 30
+    },
+    {
+      _id: 2,
+      name: 'Stretch Dress',
+      category: 'Shirts',
+      image: '/images/product0.jpg',
+      price: 99,
+      brand: 'Bar III',
+      rating: 4.5,
+      numReview: 188,
+      countInStock: 20
+    },
+    {
+      _id: 3,
+      name: 'Wool Suit Jacket',
+      category: 'suits',
+      image: '/images/product1.jpg',
+      price: 300,
+      brand: 'Banana Republic',
+      rating: 4.7,
+      numReview: 10,
+      countInStock: 35
+    },
+    {
+      _id: 4,
+      name: 'Groom Tuxedo',
+      category: 'suits',
+      image: '/images/product2.jpg',
+      price: 200,
+      brand: 'Tuxedo',
+      rating: 4.9,
+      numReview: 20,
+      countInStock: 23
+    },
+    {
+      _id: 5,
+      name: 'Casual Slim',
+      category: 'Shrits',
+      image: '/images/product3.jpg',
+      price: 60,
+      brand: 'Coton',
+      rating: 4.4,
+      numReview: 20,
+      countInStock: 17
+    },
+    {
+      _id: 6,
+      name: 'Casual Slim',
+      category: 'Shrits',
+      image: '/images/product3.jpg',
+      price: 60,
+      brand: 'Coton',
+      rating: 4.4,
+      numReview: 20,
+      countInStock: 32
+    }
+  ]
+};
