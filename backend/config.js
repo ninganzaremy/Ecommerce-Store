3ad5f77eaeaff298503cbe41c5c7e59c0bdd921b
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 13:42:48 -0400

    connecting to paypal

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ecommerce',
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/ShopPro',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-
-
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
 };
