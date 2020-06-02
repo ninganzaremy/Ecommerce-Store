dd8ee35032e2c4e3fe9784d1abb7322f344797cf
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 15:19:51 -0400

    searching and sorting features

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,8 +2,23 @@ import express from 'express';
 import Product from '../models/productModel';
 import { isAuth, isAdmin } from '../util';
 
-
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const category = req.query.category ? { category: req.query.category } : {};
+  const searchKeyword = req.query.searchKeyword ? {
+    name: {
+      $regex: req.query.searchKeyword,
+      $options: 'i'
+    }
+  } : {};
+  const sortOrder = req.query.sortOrder ?
+    (req.query.sortOrder === 'lowest' ? { price: 1 } : { price: -1 })
+    :
+    { _id: -1 };
+  const products = await Product.find({ ...category, ...searchKeyword }).sort(sortOrder);
+  res.send(products);
+});
 
 router.get("/:id", async (req, res) => {
   const product = await Product.findOne({ _id: req.params.id });
@@ -11,6 +26,36 @@ router.get("/:id", async (req, res) => {
     res.send(product);
   } else {
     res.status(404).send({ message: "Product Not Found." });
+  }
+});
+
+router.put("/:id", isAuth, isAdmin, async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findById(productId);
+  if (product) {
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.image = req.body.image;
+    product.brand = req.body.brand;
+    product.category = req.body.category;
+    product.countInStock = req.body.countInStock;
+    product.description = req.body.description;
+    const updatedProduct = await product.save();
+    if (updatedProduct) {
+      return res.status(200).send({ message: 'Product Updated', data: updatedProduct });
+    }
+  }
+  return res.status(500).send({ message: ' Error in Updating Product.' });
+
+});
+
+router.delete("/:id", isAuth, isAdmin, async (req, res) => {
+  const deletedProduct = await Product.findById(req.params.id);
+  if (deletedProduct) {
+    await deletedProduct.remove();
+    res.send({ message: "Product Deleted" });
+  } else {
+    res.send("Error in Deletion.");
   }
 });
 
@@ -34,35 +79,5 @@ router.post("/", isAuth, isAdmin, async (req, res) => {
   return res.status(500).send({ message: ' Error in Creating Product.' });
 })
 
-router.put("/:id", isAuth, isAdmin, async (req, res) => {
-  const productId = req.params.id;
-  const product = await Product.findById(productId);
-  if (product) {
-    product.name = req.body.name;
-    product.price = req.body.price;
-    product.image = req.body.image;
-    product.brand = req.body.brand;
-    product.category = req.body.category;
-    product.countInStock = req.body.countInStock;
-    product.description = req.body.description;
-    const updatedProduct = await product.save();
-    if (updatedProduct) {
-      return res
-        .status(200)
-        .send({ message: "Product Updated", data: updatedProduct });
-    }
-  }
-  return res.status(500).send({ message: " Error in Updating Product." });
-});
-
-router.delete("/:id", isAuth, isAdmin, async (req, res) => {
-  const deletedProduct = await Product.findById(req.params.id);
-  if (deletedProduct) {
-    await deletedProduct.remove();
-    res.send({ message: "Product Deleted" });
-  } else {
-    res.send("Error in Deletion.");
-  }
-});
 
 export default router;
