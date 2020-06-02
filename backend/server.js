18b31b4378b1f4f52c3e42b81f7ff49d8f059260
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 15:48:19 -0400

    Orders and Paypal configurations route added

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
 import express from 'express';
-import data from './data';
-import dotenv from 'dotenv';
-import config from './config';
-import userRoute from "./routes/userRoute";
-import productRoute from "./routes/productRoute";
-
-import mongoose  from 'mongoose';
+import path from 'path';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import config from './config';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+import orderRoute from './routes/orderRoute';
 
-
-dotenv.config();
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .catch((error) => console.log(error.reason));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).catch((error) => console.log(error.reason));
 
 
 const app = express();
 app.use(bodyParser.json());
-app.use("/api/products", productRoute);
-app.use("/api/users", userRoute);
-app.listen(5000,() => {console.log("Server started at http://localhost:5000")});
+
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(config.PAYPAL_CLIENT_ID);
+});
+
+// app.use(express.static(path.join(__dirname, '/../frontend/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+// });
+
+app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
