194162e1d6091334be5bc0f2d830f8e8375e2ec5
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 13:38:13 -0400

    payment, user INFo upadate and product edit Reducers added

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,35 +1,47 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL} from "../constants/productConstants";
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL } from "../constants/productConstants";
 
+function productListReducer(state = { products: [] }, action) {
 
-
-
-function productListReducer(state = { products: [] }, action){
-  switch(action.type){
+  switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-    return {loading: true, products: [] };
+      return { loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
-    return {loading: false, products: action.payload};
+      return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
-    return {loading: false, error:action.payload}
+      return { loading: false, error: action.payload }
     default:
-    return state;
+      return state;
   }
 }
 
-function productDetailsReducer(state = { product: {} }, action){
+function productDetailsReducer(state = { product: {} }, action) {
 
-
-  switch(action.type){
+  switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-    return {loading: true};
+      return { loading: true };
     case PRODUCT_DETAILS_SUCCESS:
-    return {loading: false, product: action.payload};
+      return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
-    return {loading: false, error:action.payload}
+      return { loading: false, error: action.payload }
     default:
-    return state;
+      return state;
   }
 }
+
+function productDeleteReducer(state = { product: {} }, action) {
+
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true };
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, product: action.payload, success: true };
+    case PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state;
+  }
+}
+
 function productSaveReducer(state = { product: {} }, action) {
 
   switch (action.type) {
@@ -44,17 +56,4 @@ function productSaveReducer(state = { product: {} }, action) {
   }
 }
 
-function productDeleteReducer(state = { product: {} }, action) {
-  switch (action.type) {
-    case PRODUCT_DELETE_REQUEST:
-      return { loading: true };
-    case PRODUCT_DELETE_SUCCESS:
-      return { loading: false, product: action.payload, success: true };
-    case PRODUCT_DELETE_FAIL:
-      return { loading: false, error: action.payload };
-    default:
-      return state;
-  }
-}
-
-export {productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer }
+export { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer }
