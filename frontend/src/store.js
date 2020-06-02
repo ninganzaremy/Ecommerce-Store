53eff6e35a48b47790acc379c54c400d0ee1a53c
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 16:04:37 -0400

    Product edit, delete, create an order, save it, edit it, features added to our store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,15 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import {
-  productListReducer,
-  productDetailsReducer,
-  productSaveReducer,
-  productDeleteReducer,
-} from "./reducers/productReducers";
-import { cartReducer } from "./reducers/cartReducers";
-import Cookie from "js-cookie";
-import { userSigninReducer } from "./reducers/userReducers";
-import { userRegisterReducer } from "./reducers/userReducers";
-
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
+import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers'
+import { userSigninReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer, myOrderListReducer, orderListReducer, orderDeleteReducer } from './reducers/orderReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
 
-
-const initialState = { cart: { cartItems } , userSignin:{userInfo}};
+const initialState = { cart: { cartItems, shipping: {}, payment: {} }, userSignin: { userInfo } };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
@@ -26,13 +18,15 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   productSave: productSaveReducer,
   productDelete: productDeleteReducer,
-});
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  userUpdate: userUpdateReducer,
+  myOrderList: myOrderListReducer,
+  orderList: orderListReducer,
+  orderDelete: orderDeleteReducer
 
+})
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
-);
-
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
