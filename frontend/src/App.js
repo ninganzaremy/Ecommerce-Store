248d297be613ee4e3b0994323974b9388f6b7328
Ningaanza Remy <remimaradona@gmail.com>
2020-06-02 15:55:53 -0400

    creating dynamic products as admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,118 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import ProductPage from "./components/ProductPage";
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css';
+import HomePage from './components/HomePage';
+import ProductPage from './components/ProductPage';
 import CartPage from './components/CartPage';
-import "./App.css";
-import SigninPage from "./components/SigninPage";
-import RegisterPage from "./components/RegisterPage";
-import { useSelector } from "react-redux";
-import ProductsPage from "./components/ProductsPage";
-
+import SigninPage from './components/SigninPage';
+import { useSelector } from 'react-redux';
+import RegisterPage from './components/RegisterPage';
+import ProductsPage from './components/ProductsPage';
+import ShippingPage from './components/ShippingPage';
+import PaymentPage from './components/PaymentPage';
+import PlaceOrderPage from './components/PlaceOrderPage';
+import OrderPage from './components/OrderPage';
+import ProfilePage from './components/ProfilePage';
+import OrdersPage from './components/OrdersPage';
 
 function App() {
-  const userSignin = useSelector(state=>state.userSignin);
-  const {userInfo}= userSignin;
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
-  };
+  }
   const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
-  };
+    document.querySelector(".sidebar").classList.remove("open")
+  }
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="header">
           <div className="brand">
-            <button type="button" onClick={openMenu}>
+            <button onClick={openMenu}>
               &#9776;
-            </button>
-            <Link to="/"> SuitsPro </Link>
+        </button>
+            <Link to="/" >ShopPro</Link>
           </div>
           <div className="header-links">
-            <a href="cart.html">cart</a>
-            {userInfo ? (
-              <Link to="/profile"> {userInfo.name}</Link>
-            ) : (
-              <Link to="./signin">Login</Link>
+          {cartItems.length !== 0 && <div className="badge">{cartItems.length}</div>}
+            <Link className="header-link" to="/cart">
+              <i className="fas fa-shopping-cart" />
+            </Link>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#"  >Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+              </div>
             )}
           </div>
         </header>
         <aside className="sidebar">
-          <h3> Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>
-            X
-          </button>
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
           <ul className="categories">
-            <a href="index.html">Suits</a>
-            <a href="index.html">Shirts</a>
-            <a href="index.html">Pants</a>
+            <li>
+              <Link to="/category/Computers">Computers</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Phones">Phones</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Printers">Printers</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Televisions">Televisions</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Cameras">Cameras</Link>
+            </li>
+
+
+
+
+
           </ul>
         </aside>
         <main className="main">
           <div className="content">
+            <Route path="/orders" component={OrdersPage} />
+            <Route path="/profile" component={ProfilePage} />
+            <Route path="/order/:id" component={OrderPage} />
             <Route path="/products" component={ProductsPage} />
+            <Route path="/shipping" component={ShippingPage} />
+            <Route path="/payment" component={PaymentPage} />
+            <Route path="/placeorder" component={PlaceOrderPage} />
             <Route path="/signin" component={SigninPage} />
             <Route path="/register" component={RegisterPage} />
             <Route path="/product/:id" component={ProductPage} />
             <Route path="/cart/:id?" component={CartPage} />
+            <Route path="/category/:id" component={HomePage} />
             <Route path="/" exact={true} component={HomePage} />
+
+
           </div>
+
         </main>
-        <footer className="footer">Remy-2020 All right reserved</footer>
+        <footer className="footer">
+        Copyright © 2020 REMY - Software Developer
+
+    </footer>
       </div>
     </BrowserRouter>
   );
