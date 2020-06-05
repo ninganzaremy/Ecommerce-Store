a3d665c66929ab3dca6c13519b0b3230e094f9b3
Ningaanza Remy <remimaradona@gmail.com>
2020-06-05 19:59:37 -0400

    Review schema added for a product rating and comment by a user

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,17 +1,37 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  brand: { type: String, required: true },
-  price: { type: Number, default: 0, required: true },
-  category: { type: String, required: true },
-  countInStock: { type: Number, default: 0, required: true },
-  description: { type: String, required: true },
-  rating: { type: Number, default: 0, required: true },
-  numReviews: { type: Number, default: 0, required: true },
-});
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const productModel = mongoose.model("Product", productSchema);
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number, default: 0, required: true },
+    category: { type: String, required: true },
+    countInStock: { type: Number, default: 0, required: true },
+    description: { type: String, required: true },
+    reviews: [reviewSchema],
+    rating: { type: Number, default: 0, min: 0, max: 5, required: true },
+    numReviews: { type: Number, default: 0, required: true },
+  },
+  { timestamps: true }
+);
 
-export default productModel;
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
