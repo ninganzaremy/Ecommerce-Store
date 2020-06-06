6f9aee2ed8595c120ee78911b2748adaeab05a31
Ningaanza Remy <remimaradona@gmail.com>
2020-06-05 20:06:08 -0400

    Route to post  Reviews set and express-async-handler added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,53 +1,71 @@
-import express from 'express';
-import Product from '../models/productModel';
-import { isAuth, isAdmin } from '../util';
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel";
+import { isAuth, isAdmin } from "../util";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const category = req.query.category ? { category: req.query.category } : {};
-  const searchKeyword = req.query.searchKeyword ? {
-    name: {
-      $regex: req.query.searchKeyword,
-      $options: 'i'
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const category = req.query.category ? { category: req.query.category } : {};
+    const searchKeyword = req.query.searchKeyword
+      ? {
+          name: {
+            $regex: req.query.searchKeyword,
+            $options: "i",
+          },
+        }
+      : {};
+    const sortOrder = req.query.sortOrder
+      ? req.query.sortOrder === "lowest"
+        ? { price: 1 }
+        : { price: -1 }
+      : { _id: -1 };
+    const products = await Product.find({ ...category, ...searchKeyword }).sort(
+      sortOrder
+    );
+    res.send(products);
+  })
+);
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findOne({ _id: req.params.id });
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Product Not Found." });
     }
-  } : {};
-  const sortOrder = req.query.sortOrder ?
-    (req.query.sortOrder === 'lowest' ? { price: 1 } : { price: -1 })
-    :
-    { _id: -1 };
-  const products = await Product.find({ ...category, ...searchKeyword }).sort(sortOrder);
-  res.send(products);
-});
+  })
+);
 
-router.get("/:id", async (req, res) => {
-  const product = await Product.findOne({ _id: req.params.id });
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found." });
-  }
-});
-
-router.put("/:id", isAuth, isAdmin, async (req, res) => {
-  const productId = req.params.id;
-  const product = await Product.findById(productId);
-  if (product) {
-    product.name = req.body.name;
-    product.price = req.body.price;
-    product.image = req.body.image;
-    product.brand = req.body.brand;
-    product.category = req.body.category;
-    product.countInStock = req.body.countInStock;
-    product.description = req.body.description;
-    const updatedProduct = await product.save();
-    if (updatedProduct) {
-      return res.status(200).send({ message: 'Product Updated', data: updatedProduct });
+router.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name;
+      product.price = req.body.price;
+      product.image = req.body.image;
+      product.brand = req.body.brand;
+      product.category = req.body.category;
+      product.countInStock = req.body.countInStock;
+      product.description = req.body.description;
+      const updatedProduct = await product.save();
+      if (updatedProduct) {
+        return res
+          .status(200)
+          .send({ message: "Product Updated", data: updatedProduct });
+      }
     }
-  }
-  return res.status(500).send({ message: ' Error in Updating Product.' });
-
-});
+    return res.status(500).send({ message: " Error in Updating Product." });
+  })
+);
 
 router.delete("/:id", isAuth, isAdmin, async (req, res) => {
   const deletedProduct = await Product.findById(req.params.id);
@@ -59,25 +77,65 @@ router.delete("/:id", isAuth, isAdmin, async (req, res) => {
   }
 });
 
+router.post(
+  "/",
+  isAuth,
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      image: req.body.image,
+      brand: req.body.brand,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      description: req.body.description,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+    });
+    const newProduct = await product.save();
+    if (newProduct) {
+      return res
+        .status(201)
+        .send({ message: "New Product Created", data: newProduct });
+    }
+    return res.status(500).send({ message: " Error in Creating Product." });
+  })
+);
 
-router.post("/", isAuth, isAdmin, async (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    price: req.body.price,
-    image: req.body.image,
-    brand: req.body.brand,
-    category: req.body.category,
-    countInStock: req.body.countInStock,
-    description: req.body.description,
-    rating: req.body.rating,
-    numReviews: req.body.numReviews,
-  });
-  const newProduct = await product.save();
-  if (newProduct) {
-    return res.status(201).send({ message: 'New Product Created', data: newProduct });
-  }
-  return res.status(500).send({ message: ' Error in Creating Product.' });
-})
-
+router.post(
+  "/:id/reviews",
+  isAuth,
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      const review = {
+        rating: req.body.rating,
+        comment: req.body.comment,
+        user: req.user._id,
+        name: req.user.name,
+      };
+      product.reviews.push(review);
+      product.rating =
+        product.reviews.reduce((a, c) => c.rating + a, 0) /
+        product.reviews.length;
+      product.numReviews = product.reviews.length;
+      const updatedProduct = await product.save();
+      res.send({
+        message: "Comment Created.",
+        data: updatedProduct.reviews[updatedProduct.reviews.length - 1],
+      });
+    } else {
+      throw Error("Product does not exist.");
+    }
+  })
+);
+router.get(
+  "/categories",
+  asyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct("category");
+    res.send(categories);
+  })
+);
 
 export default router;
