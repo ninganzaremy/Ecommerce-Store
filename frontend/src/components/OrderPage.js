32700da6796db66b4a975d8780833ed4f44ed925
Ningaanza Remy <remimaradona@gmail.com>
2020-06-05 22:12:29 -0400

    added Order Page to show to the user order details after placing an order

--- frontend/src/components/OrderPage.js
+++ frontend/src/components/OrderPage.js
@@ -1,137 +1,149 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { createOrder, detailsOrder, payOrder } from '../actions/orderActions';
-import PaypalButton from '../payComponents/PaypalButton';
-function OrderPage(props) {
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  createOrder,
+  detailsOrder,
+  payOrder,
+  deliverOrder,
+} from "../actions/orderActions";
+import PaypalButton from "../payComponents/PaypalButton";
+import LoadingBox from "../payComponents/LoadingBox";
+import ErrorBox from "../payComponents/ErrorBox";
+import { ORDER_PAY_RESET } from "../constants/orderConstants";
 
-  const orderPay = useSelector(state => state.orderPay);
-  const { loading: loadingPay, success: successPay, error: errorPay } = orderPay;
+function OrderPage(props) {
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { loading, order, error } = orderDetails;
+  const userSignin = useSelector((state) => state.userSignin);
+  const orderPay = useSelector((state) => state.orderPay);
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const {
+    loading: loadingDeliver,
+    error: errorDeliver,
+    success: successDeliver,
+  } = orderDeliver;
+
+  const {
+    loading: loadingPay,
+    success: successPay,
+    error: errorPay,
+  } = orderPay;
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/profile";
+  const { userInfo } = userSignin;
+
   const dispatch = useDispatch();
   useEffect(() => {
     if (successPay) {
+      dispatch({ type: ORDER_PAY_RESET });
       props.history.push("/profile");
     } else {
       dispatch(detailsOrder(props.match.params.id));
     }
-    return () => {
-    };
-  }, [successPay]);
+    return () => {};
+  }, [dispatch, successPay]);
 
   const handleSuccessPayment = (paymentResult) => {
     dispatch(payOrder(order, paymentResult));
-  }
+  };
+  const handleDeliverOrder = () => {
+    dispatch(deliverOrder(order));
+  };
 
-  const orderDetails = useSelector(state => state.orderDetails);
-  const { loading, order, error } = orderDetails;
-
-  return loading ? <div>Loading ...</div> : error ? <div>{error}</div> :
-
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <ErrorBox message={error} />
+  ) : (
     <div>
+      <div className="back-to-result">
+        <Link to={redirect}> ‹ Back to list</Link>
+        <br />
+        <h3>Order-id: {order._id}</h3>
+      </div>
+
       <div className="placeorder">
         <div className="placeorder-info">
           <div>
-            <h3>
-              Shipping
-          </h3>
+            <h3>Shipping Address:</h3>
             <div>
-              {order.shipping.address}, {order.shipping.city},
-          {order.shipping.postalCode}, {order.shipping.country},
-          </div>
-            <div>
-              {order.isDelivered ? "Delivered at " + order.deliveredAt : "Not Delivered."}
+              {order.shipping.address}, {order.shipping.city},{" "}
+              {order.shipping.country}, {order.shipping.postalCode}
             </div>
+            <h3>
+              Delivery Status:{" "}
+              {order.isDelivered
+                ? `Delivered At ${order.deliveredAt}`
+                : "Not Delivered"}
+            </h3>
           </div>
           <div>
-            <h3>Payment</h3>
-            <div>
-              Payment Method: {order.payment.paymentMethod}
-            </div>
-            <div>
-              {order.isPaid ? "Paid at " + order.paidAt : "Not Paid."}
-            </div>
+            <h3>Payment Method</h3>
+
+            <div>{order.payment.paymentMethod}</div>
+            <h3>
+              Payment Status:
+              {order.isPaid ? `Paid At ${order.paidAt}` : "Not Paid"}
+            </h3>
           </div>
           <div>
             <ul className="cart-list-container">
               <li>
-                <h3>
-                  Shopping Cart
-          </h3>
-                <div>
-                  Price
-          </div>
+                <h3>Order Items</h3>
+                <div>Price</div>
               </li>
-              {
-                order.orderItems.length === 0 ?
-                  <div>
-                    Cart is empty
-          </div>
-                  :
-                  order.orderItems.map(item =>
-                    <li key={item._id}>
-                      <div className="cart-image">
-                        <img src={item.image} alt="product" />
-                      </div>
-                      <div className="cart-name">
-                        <div>
-                          <Link to={"/product/" + item.product}>
-                            {item.name}
-                          </Link>
-
-                        </div>
-                        <div>
-                          Qty: {item.qty}
-                        </div>
-                      </div>
-                      <div className="cart-price">
-                        ${item.price}
-                      </div>
-                    </li>
-                  )
-              }
+              {order.orderItems.map((item) => (
+                <li key={item._id}>
+                  <div className="cart-image">
+                    <img src={item.image} alt="product" />
+                  </div>
+                  <div className="cart-name">
+                    <div>
+                      <Link to={"/product/" + item.product}>{item.name}</Link>
+                    </div>
+                    <div>Qty: {item.qty}</div>
+                  </div>
+                  <div className="cart-price">${item.price}</div>
+                </li>
+              ))}
             </ul>
           </div>
-
-
         </div>
         <div className="placeorder-action">
           <ul>
             <li className="placeorder-actions-payment">
-              {loadingPay && <div>Finishing Payment...</div>}
-              {!order.isPaid &&
+              {!order.isPaid && (
                 <PaypalButton
                   amount={order.totalPrice}
-                  onSuccess={handleSuccessPayment} />
-              }
+                  onSuccess={handleSuccessPayment}
+                />
+              )}
             </li>
+            <h3>Order Summary</h3>
+
             <li>
-              <h3>Order Summary</h3>
-            </li>
-            <li>
-              <div>Items</div>
+              <div>Items:</div>
               <div>${order.itemsPrice}</div>
             </li>
             <li>
-              <div>Shipping</div>
-              <div>${order.shippingPrice}</div>
+              <div>Shipping:</div>
+              <div>${order.shippingPrice ? `${order.shippingPrice}` : "0"}</div>
             </li>
             <li>
-              <div>Tax</div>
+              <div>Tax:</div>
               <div>${order.taxPrice}</div>
             </li>
             <li>
-              <div>Order Total</div>
+              <div>Order Total:</div>
               <div>${order.totalPrice}</div>
             </li>
           </ul>
-
-
-
         </div>
-
       </div>
     </div>
-
+  );
 }
 
 export default OrderPage;
