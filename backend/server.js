16a5de086221ffcdffc2ec859e2e58d721966f88
Ningaanza Remy <remimaradona@gmail.com>
2020-06-05 20:15:44 -0400

    File upload to the the server feature created

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import config from './config';
-import userRoute from './routes/userRoute';
-import productRoute from './routes/productRoute';
-import orderRoute from './routes/orderRoute';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
+import config from "./config";
+import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
+import orderRoute from "./routes/orderRoute";
 
+dotenv.config();
 const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).catch((error) => console.log(error.reason));
-
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/api/users', userRoute);
-app.use('/api/products', productRoute);
-app.use('/api/orders', orderRoute);
-app.get('/api/config/paypal', (req, res) => {
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
@@ -30,4 +34,6 @@ app.get('/api/config/paypal', (req, res) => {
 //   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 // });
 
-app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
+app.listen(config.PORT, () => {
+  console.log("Server started at http://localhost:5000");
+});
