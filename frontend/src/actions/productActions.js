104c0ab1dc5303a40ae90fabbf3d53af5e645233
Ningaanza Remy <remimaradona@gmail.com>
2020-06-05 21:46:41 -0400

    Products reviews and Products categories Actions created

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,49 +1,77 @@
 import {
-  PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
-  PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST
-} from "../constants/productConstants"
-import axios from 'axios';
-import Axios from "axios";
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_SAVE_REQUEST,
+  PRODUCT_SAVE_SUCCESS,
+  PRODUCT_SAVE_FAIL,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_CATEGORY_LIST_REQUEST,
+  PRODUCT_CATEGORY_LIST_SUCCESS,
+  PRODUCT_CATEGORY_LIST_FAIL,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_REVIEW_SAVE_REQUEST,
+  PRODUCT_REVIEW_SAVE_SUCCESS,
+  PRODUCT_REVIEW_SAVE_FAIL,
+} from "../constants/productConstants";
+import axios from "axios";
 
-const listProducts = (category = '', searchKeyword = '', sortOrder = '') => async (dispatch) => {
+import { getErrorMessage } from "../util";
+
+const listProducts = (
+  category = "",
+  searchKeyword = "",
+  sortOrder = ""
+) => async (dispatch) => {
   try {
-
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("/api/products?category=" + category +
-      "&searchKeyword=" + searchKeyword + "&sortOrder=" + sortOrder);
+    const { data } = await axios.get(
+      "/api/products?category=" +
+        category +
+        "&searchKeyword=" +
+        searchKeyword +
+        "&sortOrder=" +
+        sortOrder
+    );
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-  }
-  catch (error) {
-
+  } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
   }
-}
+};
 
 const saveProduct = (product) => async (dispatch, getState) => {
   try {
     dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
-    const { userSignin: { userInfo } } = getState();
+    const {
+      userSignin: { userInfo },
+    } = getState();
     if (!product._id) {
-      const { data } = await Axios.post('/api/products', product, {
+      const { data } = await axios.post("/api/products", product, {
         headers: {
-          'Authorization': 'Bearer ' + userInfo.token
-        }
+          Authorization: "Bearer " + userInfo.token,
+        },
       });
       dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
     } else {
-      const { data } = await Axios.put('/api/products/' + product._id, product, {
-        headers: {
-          'Authorization': 'Bearer ' + userInfo.token
+      const { data } = await axios.put(
+        "/api/products/" + product._id,
+        product,
+        {
+          headers: {
+            Authorization: "Bearer " + userInfo.token,
+          },
         }
-      });
+      );
       dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
     }
-
   } catch (error) {
-
     dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
   }
-}
+};
 
 const detailsProduct = (productId) => async (dispatch) => {
   try {
@@ -52,24 +80,62 @@ const detailsProduct = (productId) => async (dispatch) => {
     dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
-
   }
-}
+};
 
 const deleteProduct = (productId) => async (dispatch, getState) => {
   try {
-    const { userSignin: { userInfo } } = getState();
+    const {
+      userSignin: { userInfo },
+    } = getState();
     dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
     const { data } = await axios.delete("/api/products/" + productId, {
       headers: {
-        Authorization: 'Bearer ' + userInfo.token
-      }
+        Authorization: "Bearer " + userInfo.token,
+      },
     });
     dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data, success: true });
   } catch (error) {
     dispatch({ type: PRODUCT_DELETE_FAIL, payload: error.message });
-
   }
-}
+};
+const saveProductReview = (productId, review) => async (dispatch, getState) => {
+  dispatch({ type: PRODUCT_REVIEW_SAVE_REQUEST, payload: review });
+  try {
+    const {
+      userSignin: {
+        userInfo: { token },
+      },
+    } = getState();
+    const { data: savedReview } = await axios.post(
+      `/api/products/${productId}/reviews`,
+      review,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    dispatch({ type: PRODUCT_REVIEW_SAVE_SUCCESS, payload: savedReview });
+  } catch (error) {
+    dispatch({ type: PRODUCT_REVIEW_SAVE_FAIL, payload: error.message });
+  }
+};
+const listProductCategories = () => async (dispatch) => {
+  dispatch({ type: PRODUCT_CATEGORY_LIST_REQUEST, loading: true });
+  try {
+    const result = await axios.get("/api/products/categories");
+    dispatch({ type: PRODUCT_CATEGORY_LIST_SUCCESS, payload: result.data });
+  } catch (error) {
+    dispatch({ type: PRODUCT_CATEGORY_LIST_FAIL, payload: error.message });
+  }
+};
 
-export { listProducts, detailsProduct, saveProduct, deleteProduct }
+export {
+  listProducts,
+  detailsProduct,
+  saveProduct,
+  deleteProduct,
+  saveProductReview,
+  listProductCategories,
+};
